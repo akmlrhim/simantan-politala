22b5fa46752c995783a6b30e37780f078fe70be4
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2024-10-07 21:01:17 +0800

    setup template

---
+++ app/Views/layout/sidebar.php
@@ -0,0 +1,33 @@
+<div class="sidebar-inner slimscrollleft" id="sidebar-main">
+	<div id="sidebar-menu">
+		<ul>
+			<!-- <li class="menu-title">Main</li> -->
+
+			<li>
+				<a href="index.html" class="waves-effect">
+					<i class="mdi mdi-home"></i>
+					<span> Dashboard </span>
+				</a>
+			</li>
+
+			<li class="has_sub">
+				<a href="javascript:void(0);" class="waves-effect">
+					<i class="mdi mdi-animation"></i>
+					<span> Data Master </span>
+					<span class="float-right">
+						<i class="mdi mdi-chevron-right"></i>
+					</span>
+				</a>
+				<ul class="list-unstyled">
+					<li>
+						<a href="ui-badge.html">Badge</a>
+					</li>
+
+				</ul>
+			</li>
+
+			<!-- <li class="menu-title">Components</li> -->
+		</ul>
+	</div>
+	<div class="clearfix"></div>
+</div>
