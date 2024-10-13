e86a9facc5825615d0c91c5432aab3d4d9d38e69
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2024-10-13 20:34:16 +0800

    images profile

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -80,6 +80,13 @@
 
 	<!-- App js -->
 	<script src="<?= base_url(); ?>assets/js/app.js"></script>
+
+	<!-- Datatable js -->
+	<script src="<?= base_url(); ?>assets/plugins/datatables/jquery.dataTables.min.js"></script>
+	<script src="<?= base_url(); ?>assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+	<script src="<?= base_url(); ?>assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+	<script src="<?= base_url(); ?>assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+	<script src="<?= base_url(); ?>assets/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
 	<?= $this->renderSection('script'); ?>
 
 </body>
